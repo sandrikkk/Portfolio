cf2ae358b25e258566bfc40ed0c76006467f5820
sandrikk <iashvilisandro7@gmail.com>
2022-04-28 15:47:17 +0400

    s

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     "resumeapp/static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
